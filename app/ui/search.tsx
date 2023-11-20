9f43e4fb756094cb5a578422275a8ccdfcbbaae0
Zarif <zarifhosen2015@gmail.com>
2023-11-20 20:24:08 +0600

    bug fixed, damn coding is hard!

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -7,15 +7,15 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
-  const params = new URLSearchParams(searchParams);
   function handleSearch(term: string) {
+    const params = new URLSearchParams(searchParams);
     if (term) {
       params.set("query", term);
     } else {
       params.delete("query");
     }
+    replace(`${pathname}?${params.toString()}`);
   }
-  replace(`${pathname}?${params.toString()}`);
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
